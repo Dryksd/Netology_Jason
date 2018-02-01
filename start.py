fc5c72d0499c94390f554390070e930cdde5b6b1
Dryksd <samara.lawfirm@gmail.com>
2018-02-01 18:28:57 +0300

    Initial commit

---
+++ start.py
@@ -0,0 +1,50 @@
+import json
+
+def nok_1():
+    list_1 = []
+    with open('newsafr.json','r', encoding='utf-8') as f:
+        for line in f:
+            sect = line.split(' ')
+            for word in sect:
+                if len(word) > 6:
+                    list_1.append(word)
+    return list_1
+
+
+def nok_2():
+    list_2 = []
+    with open('newscy.json','r', encoding='KOI8-R') as f:
+        for line in f:
+            sect = line.split(' ')
+            for word in sect:
+                if len(word) > 6:
+                    list_2.append(word)
+    return list_2
+
+def nok_3():
+    list_3 = []
+    with open('newsfr.json','r', encoding='ISO-8859-5') as f:
+        for line in f:
+            sect = line.split(' ')
+            for word in sect:
+                if len(word) > 6:
+                    list_3.append(word)
+    return list_3
+
+def nok_4():
+    list_4 = []
+    with open('newsit.json','r', encoding='windows-1251') as f:
+        for line in f:
+            sect = line.split(' ')
+            for word in sect:
+                if len(word) > 6:
+                    list_4.append(word)
+    return list_4
+
+
+
+print(nok_1())
+
+
+
+#enc = chardet.detect(tek
